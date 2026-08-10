4babb8d1751475e7472c64affb69aa202f8a8689
Earthworm-jk <nkimjeeyoung@gmail.com>
2026-08-10 09:47:50 +0900

    design: load refined stage workspace styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import "./stage.css";
+import "./stage-v2.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
